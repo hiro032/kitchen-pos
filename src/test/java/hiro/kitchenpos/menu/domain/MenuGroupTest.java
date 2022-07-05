58b105c24541741c57facec84822a234ebdbb609
hiro <kiryuinh32@gmail.com>
2022-07-05 15:27:39 +0900

    test(MenuGroup): 메뉴 그룹 이름 변경

--- src/test/java/hiro/kitchenpos/menu/domain/MenuGroupTest.java
+++ src/test/java/hiro/kitchenpos/menu/domain/MenuGroupTest.java
@@ -3,10 +3,14 @@ package hiro.kitchenpos.menu.domain;
 import hiro.kitchenpos.menugroup.domain.MenuGroup;
 import hiro.kitchenpos.menugroup.domain.exception.MenuGroupNameException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class MenuGroupTest {
 
@@ -17,4 +21,25 @@ class MenuGroupTest {
         assertThatThrownBy(() -> new MenuGroup(name))
                 .isInstanceOf(MenuGroupNameException.class);
     }
+
+    @DisplayName("메뉴 그룹 생성 테스트")
+    @Test
+    void create() {
+        assertAll(
+                () -> assertDoesNotThrow(() -> new MenuGroup("한식")),
+                () -> assertThat(new MenuGroup("한식")).isInstanceOf(MenuGroup.class)
+        );
+    }
+
+    @DisplayName("메뉴 그룹 이름 변경")
+    @Test
+    void changeName() {
+        MenuGroup menuGroup = new MenuGroup("한식");
+        String newName = "일식";
+
+        menuGroup.changeMenuGroupName(newName);
+
+        assertThat(menuGroup.getName()).isEqualTo(newName);
+    }
+
 }
