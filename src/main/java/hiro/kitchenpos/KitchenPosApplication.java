d23569e77b95090c6032fe798ef2ff48a910d49d
hiro <kiryuinh32@gmail.com>
2022-07-05 09:43:07 +0900

    project init

---
+++ src/main/java/hiro/kitchenpos/KitchenPosApplication.java
@@ -0,0 +1,13 @@
+package hiro.kitchenpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenPosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hiro/kitchenpos/KitchenPosApplicationTests.java
@@ -0,0 +1,13 @@
+package hiro.kitchenpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
