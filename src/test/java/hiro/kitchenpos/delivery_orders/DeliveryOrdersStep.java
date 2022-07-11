7c3417aeecc9db7a4e34b38db4a8b0e8d15b2b01
hiro <kiryuinh32@gmail.com>
2022-07-12 00:34:48 +0900

    test: 배달 주문 서빙 기능 인수 테스트

--- src/test/java/hiro/kitchenpos/delivery_orders/DeliveryOrdersStep.java
+++ src/test/java/hiro/kitchenpos/delivery_orders/DeliveryOrdersStep.java
@@ -4,6 +4,7 @@ import hiro.kitchenpos.delivery_orders.presentation.dtos.DeliveryOrdersRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.UUID;
 import org.springframework.http.MediaType;
 
 public class DeliveryOrdersStep {
@@ -16,6 +17,15 @@ public class DeliveryOrdersStep {
                 .body(request)
                 .when()
                 .post(END_POINT)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 배달_주문_접수(final UUID orderId) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .patch(END_POINT + "/" + orderId + "/accept")
                 .then().log().all()
                 .extract();
     }
--- src/test/java/hiro/kitchenpos/delivery_orders/presentation/DeliveryOrdersControllerTest.java
+++ src/test/java/hiro/kitchenpos/delivery_orders/presentation/DeliveryOrdersControllerTest.java
@@ -3,6 +3,7 @@ package hiro.kitchenpos.delivery_orders.presentation;
 import static hiro.kitchenpos.delivery_orders.DeliveryOrderFixtures.deliveryOrdersRequest;
 import static hiro.kitchenpos.delivery_orders.DeliveryOrderFixtures.orderLineItemRequest;
 import static hiro.kitchenpos.delivery_orders.DeliveryOrdersStep.배달_주문_생성;
+import static hiro.kitchenpos.delivery_orders.DeliveryOrdersStep.배달_주문_접수;
 import static hiro.kitchenpos.menu.MenuStep.메뉴_생성;
 import static hiro.kitchenpos.menugroup.step.MenuGroupStep.메뉴_그룹_생성;
 import static hiro.kitchenpos.product.ProductsFixtures.createProductRequest;
@@ -92,6 +93,7 @@ class DeliveryOrdersControllerTest extends AcceptanceTest {
 
         UUID orderId = 배달_주문_생성(deliveryOrdersRequest).jsonPath().getUUID("id");
 
+        // Act
         ExtractableResponse<Response> response = RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
@@ -99,6 +101,7 @@ class DeliveryOrdersControllerTest extends AcceptanceTest {
                 .then().log().all()
                 .extract();
 
+        // Assert
         assertAll(
                 () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
                 () -> assertThat(response.jsonPath().getUUID("id")).isEqualTo(orderId),
@@ -106,5 +109,48 @@ class DeliveryOrdersControllerTest extends AcceptanceTest {
         );
     }
 
+    /**
+     *  Arrange
+     *  메뉴가 등록 되어 있음.
+     *  메뉴에 대한 배달 주문 요청
+     *  배달 주문에 대한 접수 요청
+     *
+     *  Act
+     *  배달 주문에 대한 서빙 요청
+     *
+     *  Assert
+     *  배달 주문이 서빙 된다빙
+     */
+    @DisplayName("배달 주문 서빙 테스트")
+    @Test
+    void delivery_order_served() {
+        // Arrange
+        UUID 치킨_콜라 = 메뉴_그룹_생성("치킨 + 콜라");
+        UUID 치킨 = 상품_생성(createProductRequest("치킨"));
+        UUID 콜라 = 상품_생성(createProductRequest("콜라"));
+
+        UUID menuId = 메뉴_생성(치킨_콜라, 치킨, 콜라);
+
+        OrderLineItemRequest orderLineItemRequest = orderLineItemRequest(menuId, 1);
+        DeliveryOrdersRequest deliveryOrdersRequest = deliveryOrdersRequest(orderLineItemRequest);
+
+        UUID orderId = 배달_주문_생성(deliveryOrdersRequest).jsonPath().getUUID("id");
+        배달_주문_접수(orderId);
+
+        // Act
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .patch(END_POINT + "/" + orderId + "/serve")
+                .then().log().all()
+                .extract();
+
+        // Assert
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(response.jsonPath().getUUID("id")).isEqualTo(orderId),
+                () -> assertThat(response.jsonPath().getString("orderStatus")).isEqualTo(DeliveryOrderStatus.SERVED.name())
+        );
+    }
 
 }
